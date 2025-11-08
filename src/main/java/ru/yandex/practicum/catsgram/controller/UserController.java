87587f270c85dbba3982eb0075d362699c571a33
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-11-08 18:41:15 +0300

    SUC-1: Класс UserService стал зависимостью для UserController

--- src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/UserController.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
 import ru.yandex.practicum.catsgram.exception.DuplicatedDataException;
 import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.User;
+import ru.yandex.practicum.catsgram.service.UserService;
 
 import java.time.Instant;
 import java.util.Collection;
@@ -14,76 +15,24 @@ import java.util.Map;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
 
-    private final Map<Long, User> users = new HashMap<>();
-    private final Map<String, Long> emailToUserId = new HashMap<>(); // для проверки уникальности email
-    private long nextId = 1;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
     public Collection<User> findAll() {
-        return users.values();
+        return userService.findAll();
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        // Проверяем необходимые условия
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            throw new ConditionsNotMetException("Email должен быть указан");
-        }
-        // проверка на уникальность
-        if (emailToUserId.containsKey(user.getEmail())) {
-            throw new DuplicatedDataException("Этот Email уже используется");
-        }
-
-        // Формируем доп. данные
-        user.setId(getNextId());
-        user.setRegistrationDate(Instant.now());
-
-        // Сохраняем нового пользователя
-        users.put(user.getId(), user);
-        emailToUserId.put(user.getEmail(), user.getId());
-
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping
     public User update(@RequestBody User newUser) {
-        // Проверяем все необходимые условия
-        if (newUser.getId() == 0) {
-            throw new ConditionsNotMetException("ID должен быть указан");
-        }
-
-        if (users.containsKey(newUser.getId())) {
-            User oldUser = users.get(newUser.getId());
-            // Проверяем уникаольность нового Email
-            if (newUser.getEmail() != null && !newUser.getEmail().equals(oldUser.getEmail())) {
-                if (emailToUserId.containsKey(newUser.getEmail())) {
-                    throw new DuplicatedDataException("Этот Email уже используется");
-                }
-
-                // Обновляем Email
-                emailToUserId.remove(oldUser.getEmail());
-                emailToUserId.put(newUser.getEmail(), newUser.getId());
-            }
-
-            // Обновляем поля если они не null
-            if (newUser.getEmail() != null) {
-                oldUser.setEmail(newUser.getEmail());
-            }
-            if (newUser.getUsername() != null) {
-                oldUser.setUsername(newUser.getUsername());
-            }
-            if (newUser.getPassword() != null) {
-                oldUser.setPassword(newUser.getPassword());
-            }
-
-            return oldUser;
-        }
-
-        throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
-    }
-
-    private long getNextId() {
-        return nextId++;
+        return userService.update(newUser);
     }
 }
