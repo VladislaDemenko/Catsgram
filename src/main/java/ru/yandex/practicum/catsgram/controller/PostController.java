80028ee7c9634527190ea416769d668b706aad17
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-11-09 12:33:20 +0300

    CPC-4: Добавлен метод на получение Id пользователя

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.catsgram.model.Post;
 import ru.yandex.practicum.catsgram.service.PostService;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/posts")
@@ -18,6 +19,11 @@ public class PostController {
     @GetMapping
     public Collection<Post> findAll() {
         return postService.findAll();
+    }
+
+    @GetMapping("/{postId}")
+    public Optional<Post> findById(@PathVariable long postId) {
+        return postService.findById(postId);
     }
 
     @PostMapping
