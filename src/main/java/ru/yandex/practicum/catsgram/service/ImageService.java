7746941b312b162402749f2b842500fba8e97558
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-11-12 00:42:30 +0300

    SIS-1: Добавлена логика возврата изображения по ID поста

---
+++ src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.catsgram.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.catsgram.model.Image;
+import ru.yandex.practicum.catsgram.model.Post;
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+    private final Map<Long, Image> images = new HashMap<>();
+
+    public List<Image> getPostImage(long postId) {
+        return images.values()
+                .stream()
+                .filter(image -> image.getPostId() == postId)
+                .collect(Collectors.toList());
+    }
+}
