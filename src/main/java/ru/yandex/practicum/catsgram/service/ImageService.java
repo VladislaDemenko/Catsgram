5c7e3567da67a367e1d257a8878cd27aa44db37b
Vladislav Demenko <vvladislademenko@yandex.ru>
2025-11-12 10:55:42 +0300

    SPS-5: Реализована загрузка файлов с диска

--- src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/ImageService.java
@@ -7,8 +7,11 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ru.yandex.practicum.catsgram.exception.ConditionsNotMetException;
+import ru.yandex.practicum.catsgram.exception.ImageFileException;
+import ru.yandex.practicum.catsgram.exception.NotFoundException;
 import ru.yandex.practicum.catsgram.model.Image;
 import ru.yandex.practicum.catsgram.model.Post;
+import ru.yandex.practicum.catsgram.model.ImageData;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,7 +26,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ImageService {
-    PostService postService;
+    private  final PostService postService;
     private final Map<Long, Image> images = new HashMap<>();
     private final String imageDirectory = "C:\\Users\\user\\images";
 
@@ -92,5 +95,32 @@ public class ImageService {
                 .max()
                 .orElse(0);
         return ++currentMaxId;
+    }
+
+    public ImageData getImageData(long imageId) {
+        if (!images.containsKey(imageId)) {
+            throw new NotFoundException("Изображение с id = " + imageId + " не найдено");
+        }
+
+        Image image = images.get(imageId);
+        // Загрузка файлов с диска
+        byte[] data = loadFile(image);
+
+        return new ImageData(data, image.getOriginalFileName());
+    }
+
+    private byte[] loadFile(Image image) {
+        Path path = Paths.get(image.getFilePath());
+        if (Files.exists(path)) {
+            try {
+                return Files.readAllBytes(path);
+            } catch (IOException e) {
+                throw new ImageFileException("Ошибка чтения файла. Id: " + image.getId()
+                        + ", name: " + image.getOriginalFileName(), e);
+            }
+        } else {
+            throw new ImageFileException("Файл не найден. Id: " + image.getId()
+                    + ", name: " + image.getOriginalFileName());
+        }
     }
 }
